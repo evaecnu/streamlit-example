ab60ab4fbc07eeed7160deecb9c66f97245dc11f
evaecnu <helloevalee@gmail.com>
2023-06-28 16:58:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ import math
 import pandas as pd
 import streamlit as st
 
-
+def show_result(x):
+   st.write(x)
 
 with st.form("daban",clear_on_submit=True):
    choice= st.selectbox('if the xpy in blacklist?',('yes','no'))
@@ -13,6 +14,5 @@ with st.form("daban",clear_on_submit=True):
    result=st.json({'choice':choice,'xueli':xueli,'job':job})
    submitted=st.form_submit_button("submit", on_click=show_result(result))
    
-def show_result(x):
-   st.write(x)
+
     
