68208871a8161b97817b7551a1317db6a0bc0a78
evaecnu <helloevalee@gmail.com>
2023-06-28 16:54:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ with st.form("daban",clear_on_submit=True):
    choice= st.selectbox('if the xpy in blacklist?',('yes','no'))
    xueli= st.selectbox('xpy degree?',('primary school','middle school'))
    job=st.text_input('xpy job')
-   submitted=st.form_submit_button('submit')
    result=st.json({'choice':choice,'xueli':xueli,'job':job})
-   st.write(result)
+   submitted=st.form_submit_button('submit',on_click=show_result(result))
+def show_result(x):
+   st.write(x)
     
