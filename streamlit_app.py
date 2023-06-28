0e7892ae3ecf0811575bfb5f3531f09ba135feb9
evaecnu <helloevalee@gmail.com>
2023-06-28 16:39:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,8 @@ import streamlit as st
 
 with st.form("daban",clear_on_submit=True):
    choice= st.selectbox('if the xpy in blacklist?',('yes','no'))
+   xueli= st.selectbox('xpy degree?',('primary school','middle school'))
+   job=st.text_input('xpy job')
    st.write('youselect,choice')
+   submitted=st.form_submit_button('submit')
     
