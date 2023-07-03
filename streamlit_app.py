93ddd3c00c720d92317e4f3a1bd9b9a38b078195
evaecnu <helloevalee@gmail.com>
2023-07-03 16:15:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
 
-"""
+
 data = {'group':['v', 'b', 'o','b','v','o','b','v','b',0o','v', 'b', 'o','b','v','o','b','v','b','o','v', 'b', 'o','b','v','o','b','v','b','o','v', 'b', 'o','b','v','o','b','v','b','o','b', 'o','b','v','o','b','v','b','o'],
         'Live':['A','A','C','B','A','C','B','A','B','A','A','C','B','A','C','B','A','B','A','B','C','B','A','C','B','A','B','A','A','C','A','C','B','A','B','A','B','C','B','A','C','B','A','B','A','A','C','B','A','C'] }
 df= pd.DataFrame(data)
