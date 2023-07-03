a52816453c56af5e627b4cb8f90424e4e81b7aac
evaecnu <helloevalee@gmail.com>
2023-07-03 16:27:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ data = {'group':['v','b','o','b','v','o','b','v','b','o','v', 'b','o','b','v','o
         'Live': ['A','A','C','B','A','C','B','A','B','A','A','C', 'B','A','C','B','A','B','A','B','C', 'B','A','C','B','A','B','A','A','C','A','C','B','A','B','A','B','C','B','A','C','B','A','B','A','A','C','B','A','C','B'] }
 df= pd.DataFrame(data)
 print(df)
-df1=df.groupby('group','Live').size()
+df1=df.groupby('group','Live')
 print(df1)
 st.dataframe(df)
