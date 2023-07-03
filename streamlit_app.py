b2f29ec08475da0872d13cca3f80fc12a6ba47a6
evaecnu <helloevalee@gmail.com>
2023-07-03 16:36:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import streamlit as st
 data = {'group':['v','b','o','b','v','o','b','v','b','o','v', 'b','o','b','v','o','b','v','b','o','v', 'b','o','b','v','o','b','v','b','o','v','b','o','b','v','o','b','v','b','o','b','o','b','v','o','b','v','b','o','o','b'],
         'Live': ['A','A','C','B','A','C','B','A','B','A','A','C', 'B','A','C','B','A','B','A','B','C', 'B','A','C','B','A','B','A','A','C','A','C','B','A','B','A','B','C','B','A','C','B','A','B','A','A','C','B','A','C','B'] }
 df= pd.DataFrame(data)
-print(df)
+# print(df)
 df1=df.groupby(['group','Live'], as_index=False)
 print(df1)
 st.dataframe(df)
