93fbb982170a26f0e3e4d0cf1710209e9b6160fb
evaecnu <helloevalee@gmail.com>
2023-07-03 16:24:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ import streamlit as st
 
 
 
-data = {'group':['v', 'b', 'o','b','v','o','b','v','b','o','v', 'b', 'o','b','v','o','b','v','b','o','v', 'b', 'o','b','v','o','b','v','b','o','v', 'b', 'o','b','v','o','b','v','b','o','b', 'o','b','v','o','b','v','b','o'],
-        'Live':['A','A','C','B','A','C','B','A','B','A','A','C','B','A','C','B','A','B','A','B','C','B','A','C','B','A','B','A','A','C','A','C','B','A','B','A','B','C','B','A','C','B','A','B','A','A','C','B','A','C'] }
+data = {'group':['v','b','o','b','v','o','b','v','b','o','v', 'b','o','b','v','o','b','v','b','o','v', 'b','o','b','v','o','b','v','b','o','v','b','o','b','v','o','b','v','b','o','b','o','b','v','o','b','v','b','o'],
+        'Live': ['A','A','C','B','A','C','B','A','B','A','A','C', 'B','A','C','B','A','B','A','B','C', 'B','A','C','B','A','B','A','A','C','A','C','B','A','B','A','B','C','B','A','C','B','A','B','A','A','C','B','A'] }
 df= pd.DataFrame(data)
 print(df)
 st.dataframe(df)
