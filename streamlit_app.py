fb815bcbb51145f6b87c8b1a3eedd3ec7d0160a3
evaecnu <helloevalee@gmail.com>
2023-07-03 16:12:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 from collections import namedtuple
 import altair as alt
-import math
 import pandas as pd
 import streamlit as st
+import plotly.graph_objects as go
 
-def show_result(x):
-   st.write(x)
+"""
+data = {'group':['v', 'b', 'o','b','v','o','b','v','b',0o','v', 'b', 'o','b','v','o','b','v','b','o','v', 'b', 'o','b','v','o','b','v','b','o','v', 'b', 'o','b','v','o','b','v','b','o','b', 'o','b','v','o','b','v','b','o'],
+        'Live':['A','A','C','B','A','C','B','A','B','A','A','C','B','A','C','B','A','B','A','B','C','B','A','C','B','A','B','A','A','C','A','C','B','A','B','A','B','C','B','A','C','B','A','B','A','A','C','B','A','C'] }
+df= pd.DataFrame(data)
+print(df)
+st.dataframe(df)
 
 with st.form("daban",clear_on_submit=True):
    choice= st.selectbox('if the xpy in blacklist?',('yes','no'))
