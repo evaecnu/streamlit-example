48c6bf2cdb57858b6c54c9f98a2195f9d261b683
evaecnu <helloevalee@gmail.com>
2023-07-03 16:47:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ data = {'group':['v','b','o','b','v','o','b','v','b','o','v', 'b','o','b','v','o
 df= pd.DataFrame(data)
 # print(df)
 
-df1=df.groupby('group').size()
+df1=df.groupby(df.columns.tolist(), as_index=False).size()
+
 print(df1)
-st.dataframe(df)
+#st.dataframe(df)
