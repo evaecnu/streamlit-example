0f99c8a3939be4ab7ffa31a4b743232e752cb303
evaecnu <helloevalee@gmail.com>
2023-07-06 14:13:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,14 @@ data = {
 }
 
 df = pd.DataFrame(data)
-df.index = ['A一个人住', 'B与肉体家人合住', 'C与外邦和猪', 'D与GF果子和猪', 'E与冬园物料合住', 'F与圣徒家人合住', 'G其他']
+df.index = ['A 一个人住', 'B 与肉体家人合住', 'C 与外邦合住', 'D 与GF果子合住', 'E 与冬园物料合住', 'F 与圣徒家人合住', 'G 其他']
 st.dataframe(df)
 
 cols = st.columns([1, 1])
 with cols[0]:
     team_type = st.selectbox('球队', ['YUY', 'L', 'O'])
 
-    fig = px.pie(df, values=team_type, names=['A一个人住', 'B与肉体家人合住', 'C与外邦和猪', 'D与GF果子和猪', 'E与冬园物料合住', 'F与圣徒家人合住', 'G其他'],
+    fig = px.pie(df, values=team_type, names=['A一个人住', 'B与肉体家人合住', 'C与外邦合住', 'D与GF果子合住', 'E与冬园物料合住', 'F与圣徒家人合住', 'G其他'],
                  title=f' {team_type} 队居住情况',
                  height=300, width=200)
     fig.update_layout(margin=dict(l=20, r=20, t=30, b=0), )
